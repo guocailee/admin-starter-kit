936242d16aa8ce6e76e8de252206681a675d79a0
gagelee.li <gagelee.li@zkteco.com>
2016-01-08 16:53:10 +0800

    add some config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: './app/script/app.jsx',
+    output: {
+        // Absolute output directory
+        path: __dirname + '/build',
+
+        // Output path from the view of the page
+        // Uses webpack-dev-server in development
+        publicPath: 'http://localhost:8080/',
+
+        // Filename for entry points
+        // Only adds hash in build mode
+        filename: '[name].bundle.js',
+
+        // Filename for non-entry points
+        // Only adds hash in build mode
+        chunkFilename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader!postcss-loader"
+        }, {
+            test: /\.scss$/,
+            loader: "style-loader!css-loader!postcss-loader!sass-loader"
+        }, {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    postcss: function() {
+        return [autoprefixer(
+            [
+                'ie >= 10',
+                'ie_mob >= 10',
+                'ff >= 30',
+                'chrome >= 34',
+                'safari >= 7',
+                'opera >= 23',
+                'ios >= 7',
+                'android >= 4.4',
+                'bb >= 10'
+            ]
+        )];
+    }
+}
