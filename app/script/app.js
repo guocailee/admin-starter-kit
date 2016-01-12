22884647c8c701bf753b03cf0b4cb75fb518ee99
gagelee.li <gagelee.li@zkteco.com>
2016-01-12 14:09:25 +0800

    add gulpfile

--- app/script/app.js
+++ app/script/app.js
@@ -1,3 +1,4 @@
 import angular from 'angular';
-
+import angularAnimate from 'angular-animate';
+import angularMaterial from 'angular-material';
 angular.module('appsssss', []);
---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+'use strict';
+import gulp from 'gulp';
+import del from 'del';
+import browserSync from 'browser-sync';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import pkg from './package.json';
+import packConfig from './webpack.config.js';
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+let webpackConfig = Object.create(packConfig);
+
+// Lint JavaScript
+gulp.task('lint', () =>
+    gulp.src('app/scripts/**/*.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+);
+// Copy all files at the root level (app)
+gulp.task('copy', () =>
+    gulp.src([
+        'app/*',
+        '!app/*.html'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('build'))
+    .pipe($.size({
+        title: 'copy'
+    }))
+);
+
+// Clean output directory
+gulp.task('clean', cb => del(['.tmp', 'build/*', '!build/.git'], {
+    dot: true
+}));
+gulp.task("webpack", (callback) => {
+    // run webpack
+    webpack(webpackConfig, (err, stats) => {
+        if (err) throw new $.util.PluginError("webpack", err);
+        $.util.log("[webpack]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
+
+gulp.task("webpack-dev-server", function(callback) {
+    // Start a webpack-dev-server
+    var compiler = webpack(webpackConfig);
+
+    new WebpackDevServer(compiler, {
+        publicPath: "/" + webpackConfig.output.publicPath,
+        stats: {
+            colors: true
+        }
+    }).listen(8080, "localhost", function(err) {
+        if (err) throw new gutil.PluginError("webpack-dev-server", err);
+        $.util.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+});
