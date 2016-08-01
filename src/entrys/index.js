e24199f7b117e3a6d5719771c3a426f6bfb125f8
gagelee.li <gagelee.li@zkteco.com>
2016-08-01 18:04:43 +0800

    update header

--- src/entrys/index.js
+++ src/entrys/index.js
@@ -4,11 +4,17 @@ import 'font-awesome-webpack'
 // 引入依赖
 import 'angular'
 import 'angular-animate'
+import 'angular-route'
+import 'angular-translate'
+import 'angular-ui-bootstrap'
 
 import './../app/theme/components/header/header.module.js'
 import './../app/theme/components/footer/footer.module.js'
 
 angular.module('zk.app', [
+  'ngAnimate',
+  'pascalprecht.translate',
+  'ui.bootstrap',
   'zk.theme.components.header',
   'zk.theme.components.footer'
 ])
