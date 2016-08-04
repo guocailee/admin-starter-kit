3510bf22adad3d6be2b994ed1a40c6701192a650
gagelee.li <gagelee.li@zkteco.com>
2016-08-04 17:59:05 +0800

    update header

--- src/app/theme/components/header/header.directive.js
+++ src/app/theme/components/header/header.directive.js
@@ -3,16 +3,16 @@
  *  @Date   2016-07-20
  */
 import headerHtml from 'ngtemplate!html!./header.html'
-export default function () {
+export default function ($window) {
+  'ngInject'
   return {
     restrict: 'E',
     templateUrl: headerHtml,
     link: function ($scope) {
-      var bodyDom = document.getElementsByTagName('body')[0]
-      var scrollFun = function () {
-        $scope.isScrolled = !bodyDom.scrollTop === 0
-      }
-      bodyDom.addEventListener('scroll', scrollFun)
+      angular.element($window).bind('scroll', function () {
+        $scope.isScrolled = this.pageYOffset !== 0
+        $scope.$apply()
+      })
     }
   }
 }
